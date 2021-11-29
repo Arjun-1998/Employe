8ef14f0449ebf93c29fba93e35ac839b9da1b049
Arjun-1998 <arjunjaiswal8065@gmail.com>
2021-11-29 16:38:13 +0530

    [Arjun 2] Add ; Added Classed

--- LineLengthComparison/Program.cs
+++ LineLengthComparison/Program.cs
@@ -2,3 +2,8 @@
 LineLengthComparison lineLengthComparison = new LineLengthComparison();
 double length = lineLengthComparison.CalculateLength(5, 4, 6, 8);
 Console.WriteLine(length);
+int a1 = 4, b1 = 4,
+                    c1 = 3, a2 = -6,
+                    b2 = 2, c2 = 9;
+
+lineLengthComparison.Compare(a1, b1, c1, a2, b2, c2);
