63c5ce71cb3f3d44d80a10c3cda54a925609a07d
Arjun-1998 <arjunjaiswal8065@gmail.com>
2021-11-29 16:23:11 +0530

    [Arjun 1] Add ; Added Switched

---
+++ LineLengthComparison/Program.cs
@@ -0,0 +1,4 @@
+﻿using Employe;
+LineLengthComparison lineLengthComparison = new LineLengthComparison();
+double length = lineLengthComparison.CalculateLength(5, 4, 6, 8);
+Console.WriteLine(length);
